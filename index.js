09c288b2d0ad3a7d17c6247c539b9743a27e58cc
Matt <aboutmattlaw@gmail.com>
2021-09-10 12:35:09 -0400

    commit tree b3a11c5240413d5524b7c841f3bbff81286b5a66

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Add your code here
+// // Add your code here
+
+
+
+// const formData = {
+//     dogname: "Page",
+//     dogBreed: "Piano"     
+// };
+
+// const configurationObject = {
+//     method: "Post",
+//     headers: {
+//         "Content-Type": "application/json",
+//     Accept: "applicatoon/json",
+//     },
+//     body: JSON.stringify(formData),
+// };   
+
+
+// fetch("http://localhost:3000/dogs" , configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things!");
+//         console.log(error.message);
+//     });
+
+
+
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com"     
+};
+
+function submitData (name, email){
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+};   
+
+fetch("http://localhost:3000/users" , configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function (e) {
+        // if status 409, then below
+       document.body.innerHTML = response.statusText
+    });
+
+}
+
+
